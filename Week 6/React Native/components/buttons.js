f690d4177efbae813277f8199be53f9c1f92db52
Sam <74021826+x39OME@users.noreply.github.com>
2023-09-20 19:02:41 +0300

    Add files via upload

---
+++ Week 6/React Native/components/buttons.js
@@ -0,0 +1,10 @@
+{/* <Button onPress={()=>{ale()}} title='press' color={"red"}></Button> */}
+    {/* <TouchableOpacity onPress={()=>{ale()}} activeOpacity={0.1} style={{width:"50%",backgroundColor:"blue",padding:20,borderRadius:10,marginLeft:20}}>
+      <Text style={{color:"white",textAlign:"center"}}> press</Text>
+    </TouchableOpacity> */}
+    {/* <TouchableWithoutFeedback onPress={()=>{console.log("image")}}>
+      <Image source={require("./assets/favicon.png")} style={{width:150,height:150}}></Image>
+    </TouchableWithoutFeedback> */}
+    // <Pressable style={({pressed})=>[{backgroundColor:(pressed)?"green":"yellow"},{padding:18}]}>
+    // <Text style={{color:"black",textAlign:"center"}}> press</Text>
+    // </Pressable>
